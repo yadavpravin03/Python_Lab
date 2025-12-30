64cf8c40a2115a09ee51cb2b28444b9561ace576
yadavpravin03 <yadav96625@gmail.com>
2025-12-30 23:37:32 +0530

    Add files via upload

---
+++ OPERATOR.py
@@ -0,0 +1,42 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # 1.Assignment Operator
+... x = 10
+... print(x)
+... 
+... # 2.urnary minus+
+... a=5
+... print(-a) 
+... 
+... # 3.relational
+... p=10
+... q=5
+... print(p>q)
+... print(p==q)
+... 
+... # 4.Logical
+... x=True
+... y=False
+... print(x and y)
+... print(x or y)
+... print(not x)
+... 
+... # 5.boolean 
+... is_pass= True
+... print(is_pass)  o/p true
+... 
+... # 6.bitwise
+... a= 5
+... b= 3
+... print(a & b)
+... print(a | b)
+... 
+... # 7.membership 
+... number = [1,2,3,4,5,6]
+... print(2 in number)
+... print(5 not in number)
+... 
+... # 8.identity
+x= 10
+y= 10
+print(x is y ) #o/p true
