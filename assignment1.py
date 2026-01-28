a7fbff798a4dd53b5e607fb01dac9117e327ae1c
yadavpravin03 <yadav96625@gmail.com>
2026-01-28 07:53:20 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,73 @@
+# 1.print welcome message
+
+print("welcome")
+
+# 2. address
+
+print("\tRajesh Kumar \n\tFlat no. 101,Sunshine Apartments \n\tMG Road, Sector 15 \n\tRajkot, \n\tPincode:360004 \n\tIndia.")
+
+# 3. basic mathematical operators
+
+a=150
+b=120
+
+print("additin:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+
+# 4.area of circle
+
+radius=input("enter radius of circle :")
+area=3.14*radius*radius
+circumstance=2*3.14*radius
+print("area of circle is :",area)
+print("\ncircumstance of circle is :",circumstance)
+
+# 5.simple interst 
+
+P = input("Enter Principal amount: ")
+R = input("Enter Rate of interest: ")
+T = input("Enter Time (years): ")
+
+SI = (P * R * T) / 100
+
+print("Simple Interest =", SI)
+
+#6. perimeter of rectangle
+
+length = input("Enter length: ")
+width = input("Enter width: ")
+
+perimeter = 2 * (length + width)
+
+print("Perimeter of Rectangle =", perimeter)
+
+# 7. area ofrectangle amd perimeter
+
+l=input("enter length of rectangle :")
+w=input("enter width of rectangle :")
+area=l*w
+perimeter=2*(l+w)
+print("\n\tarea ofrectangle is :",area)
+print("\n\tperimeter of rectangle is :",perimeter)
+
+# 8. perimeter of a traingle 
+
+l=input("enter length of triangle :")
+b=input("enter breadth of triangle :")
+h=input("enter height of triangle :")
+perimeter=l+b+h
+print("\n\tperimeter of triangle is :",perimeter)
+
+# 9.area of square
+
+s=input("enter side of square :")
+area=s*s
+print("\n\tarea of square is :",area)
+
+# 10. perimeter of square
+
+s=input("enter the side of square :")
+perimeter=4*s
+print("\n\tperimeter of square is :",perimeter)
