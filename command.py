9f6f5e086e61c8792d044cb2f8793daac9cf7755
yadavpravin03 <yadav96625@gmail.com>
2026-02-11 00:02:38 +0530

    Add files via upload

---
+++ command.py
@@ -0,0 +1,18 @@
+import sys
+print(sys.argv)
+
+"""String 
+D:\Python> python command.py Yadav Pravin
+['command.py', 'Yadav', 'Pravin']
+
+Integer
+PS D:\Python> python command.py 10 20 30        
+['command.py', '10', '20', '30']
+
+Float
+PS D:\Python> python command.py 61.01 1.85 69.9
+['command.py', '61.01', '1.85', '69.9']
+
+bool
+PS D:\Python> python command.py True          
+['command.py', 'True']"""
