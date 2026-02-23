789dbc21e24087757b51437f9bf419a059b84235
yadavpravin03 <yadav96625@gmail.com>
2026-02-23 11:22:43 +0530

    Add files via upload

---
+++ While.py
@@ -0,0 +1,32 @@
+#print 1 to 5
+
+i=1
+while i <= 5:
+    print(i)
+    i=i+1
+    
+    
+#print 1 to 10
+
+i=1
+while i<= 10:
+    print(i)
+    i=i+1
+
+#sum of first n natural numbers
+
+n=int(input("enter number :"))
+i=1
+s=0
+while i <=n:
+    s=s+1
+    i=i+1
+    print("sum =",s)
+
+#Table of a number 
+
+num=int(input("Enter number :"))
+i=1 
+while i<=10:
+    print(num,"X",i,"=",num*i)
+    i=i+1 
