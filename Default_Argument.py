63f184da40eeac1b971bd1f9faa7c4a87774f44a
yadavpravin03 <yadav96625@gmail.com>
2026-03-18 08:30:35 +0530

    Add files via upload

---
+++ Default_Argument.py
@@ -0,0 +1,11 @@
+# Example 1
+def greet (Name="greet"):
+    print("Hello",Name)
+greet("Pravin")
+greet()
+
+#Example 2
+def add(a,b=5):
+    print("Sum",a+b)
+add(10,20)
+add(10)
---
+++ Keyword_Argument.py
@@ -0,0 +1,5 @@
+#Ex: Basic keyword Argument
+def Simple_Interest(p:float,t:float,r:float):
+    si=(p*t*r)/100
+    print("Simple_Interest :",si)
+Simple_Interest(p=10000,t=2.5,r=3.5)
